078a146b982c2afe95d96579595e4bf230a26099
Jeremiah O'Neil <jeremiah.d.oneil@gmail.com>
2016-04-27 18:45:40 -0400

    fixed my goofs in SA experiment and set up parameter writing. Added stats.py; currently just reduces results to single csv, with max min avg costs and avg runtimes.

--- SA-SCP/main.py
+++ SA-SCP/main.py
@@ -13,7 +13,14 @@ import heuristic.main as heuristic
 def main():
     nruns = 1
     problems = sorted(library)
-    # problems = ['scp41']
+    # problems = ['scpcyc10']
+    paramdict = {'problem': list(),
+                 'size': list(),
+                 'T_init': list(),
+                 'T_end': list(),
+                 'ndrop': list(),
+                 'activity_init': list(),
+                 'coolrate': list()}
 
     for problem_name in problems:
         procproblem = process_problem(problem_name, w=False, v=True)
@@ -24,49 +31,63 @@ def main():
             size += len(s)
         print('Size: {}'.format(size))
 
-        T_init = np.mean(procproblem['costs']) * nsets
+        T_init = np.average(procproblem['costs']) * np.log2(size)
         ndrop = int(np.ceil(0.2 * nsets * nelems / size))
         # consider approxcost/meansetweight
-        print ndrop
         activity_init = nelems
         stages = [(0.05*T_init, 0.8)]
         # stages = [(0.2, 0.8), (0.05, 0.9)]
         # each stage specifies cooling rate and stage end temperature, and
         #   stages supports extension to change cooling function and to swap
         #   workpieces in parallel tempering or adjust localsearch parameters
-
-        local_searcher = heuristic_sampler(ndrop, **procproblem)
-        local_searcher.send(None)
-
-        activity_func = lambda x: activity_init * x / T_init
-        feasible = init_feasible(**procproblem)
-        workpiece = MH_sampler(feasible, activity_func, local_searcher)
-        workpiece.send(None)
-
-        oven = oven_gen(T_init)
-        oven.send(None)
+        paramdict['problem'].append(problem_name),
+        paramdict['size'].append(size),
+        paramdict['T_init'].append(T_init),
+        paramdict['T_end'].append(stages[0][0]),
+        paramdict['ndrop'].append(ndrop),
+        paramdict['activity_init'].append(activity_init),
+        paramdict['coolrate'].append(stages[0][1])
 
         times = []
         costs = []
+        iters = []
         for _ in xrange(nruns):
+            local_searcher = heuristic_sampler(ndrop, **procproblem)
+            local_searcher.send(None)
+
+            activity_func = lambda x: activity_init * x / T_init
+            feasible = init_feasible(**procproblem)
+            workpiece = MH_sampler(feasible, activity_func, local_searcher)
+            workpiece.send(None)
+
+            oven = oven_gen(T_init)
+            oven.send(None)
+
+            iteration = 1
             start_time = timeit.default_timer()
 
             for T_end, coolrate in stages:
-                T = oven.send((lambda x: coolrate*x, workpiece))
+                T = oven.send((lambda x: coolrate*x, workpiece))[0]
                 while T > T_end:
                     T, feasible = oven.next()
+                    iteration += 1
 
             stop_time = timeit.default_timer()
             runtime = stop_time - start_time
             times.append(runtime)
             cost = np.sum(procproblem['costs'][feasible['sets']])
             costs.append(cost)
+            iters.append(iteration)
             print(cost, runtime)
         print('---')
 
-        df = pd.DataFrame.from_dict({'cost': costs,
-                                     'time': times})
-        df.to_csv('out/'+problem_name+'.csv')
+        # resultsdf = pd.DataFrame.from_dict({'cost': costs,
+        #                                     'time': times,
+        #                                     'iters': iters})
+        # resultsdf.to_csv('out/'+problem_name+'.csv')
+
+    paramdf = pd.DataFrame.from_dict(paramdict)
+    paramdf.to_csv('out/parameters.csv')
 
 
 def process_problem(problem_name, **kwargs):
